80fac76f57bbb64b501ac7bbfa43e71d910980cf
Muhab Magdy <muuhaab97@gmail.com>
2025-11-25 14:25:21 +0200

    solve bug of starting app

--- docker/myrag/entrypoint.sh
+++ docker/myrag/entrypoint.sh
@@ -5,3 +5,7 @@ echo "Running database migrations..."
 cd /app/models/db_schemes/myrag/
 alembic upgrade head
 cd /app
+
+echo "Starting FastAPI server..."
+# Use exec "$@" to run CMD from Dockerfile (uvicorn)
+exec "$@"
